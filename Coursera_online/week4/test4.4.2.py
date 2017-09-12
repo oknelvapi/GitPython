288411dfbc1f8372d446d2fc84036672e113eb6f
Ihor P <eldhim@gmail.com>
2017-09-12 17:08:21 +0300

    first commit

---
+++ Coursera_online/week4/test4.4.2.py
@@ -0,0 +1,14 @@
+# Отрицательная степень
+
+
+def power(a, n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return a * 1 / power(a, abs(n)+1)
+    return a * power(a, n-1)
+
+
+a = int(input())
+n = int(input())
+print(power(a, n))
